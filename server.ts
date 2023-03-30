c1e13e43e09eda8a7df4797204759498d90aad30
jumanazar7 <68016071+bekzod818@users.noreply.github.com>
2023-03-30 11:50:17 +0500

    aside  + static

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { engine } from 'express-handlebars'
 
 const server = express()
 
+server.use('/', express.static('./public'))
 server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
@@ -13,7 +14,25 @@ server.set('view engine', '.hbs')
 server.set('views', './pages')
 
 server.get('/', (request, response) => {
-    response.render('index')
+    response.redirect('/dashboard')
+})
+
+server.get('/dashboard', (request, response) => {
+    response.render('dashboard', {
+        dashboard: true
+    })
+})
+
+server.get('/products', (request, response) => {
+    response.render('products', {
+        products: true
+    })
+})
+
+server.get('/sales', (request, response) => {
+    response.render('sales', {
+        sales: true
+    })
 })
 
 server.listen(8080, () => console.log("Server is running on port 8080"))
